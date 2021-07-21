dee6300701b07a523efad12152915374dd3f6684
김민주 <w2023@e-mirim.hs.kr>
2021-07-21 10:03:34 +0900

    Completion Commit

--- app/src/main/java/kr/hs/emirim/w2023/project10_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2023/project10_3/MainActivity.java
@@ -2,7 +2,10 @@ package kr.hs.emirim.w2023.project10_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("Activity Life Cycle", "OnCreate()");
 
         Button btnCall = findViewById(R.id.btn_call);
         Button btnFinish = findViewById(R.id.btn_finish);
@@ -24,10 +28,38 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             switch (v.getId()){
                 case R.id.btn_call:
+                    Uri uri = Uri.parse("tel:01033334444");
+                    Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                    startActivity(intent);
                     break;
                 case R.id.btn_finish:
+                    finish();
                     break;
             }
         }
     };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Activity Life Cycle", "OnDestroy()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Activity Life Cycle", "onRestart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Activity Life Cycle", "OnResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Activity Life Cycle", "OnPause()");
+    }
 }
